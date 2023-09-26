2ca798160b76f80dd5dddb5c1420948af605f65d
GSAlbos <gypsyalbos17@gmail.com>
2023-09-26 20:26:55 +0800

    Add python file from GSA
    Python

---
+++ prog_02.py
@@ -0,0 +1,8 @@
+First_Number = int(input('Enter First Number :'))
+Second_Number = int(input('Enter Second Number :'))
+
+result = First_Number - Second_Number
+print(str(First_Number) + '-' + str(Second_Number) + '=' + str(result))
+
+print(result)
+#one function calculator
